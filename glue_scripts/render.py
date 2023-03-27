98fab24c7356fbb00fe85c670a06bf73a491cccb
louis.devictoria <telescope40@gmail.com>
2023-03-27 11:11:43 -0400

    update render + vars

--- glue_scripts/render.py
+++ glue_scripts/render.py
@@ -1,33 +1,57 @@
 #Louis DeVictoria
 #Python Script to load the dictionary file and jinja2 template to create a configuration
 import sys
-
-from jinja2 import Environment, FileSystemLoader, StrictUndefined
+import os
+from jinja2 import Environment, FileSystemLoader, StrictUndefined, ChoiceLoader
 #Local Directory
-file_loader = FileSystemLoader('')
+file_loader = FileSystemLoader('.')
 #Load Environment
 env = Environment(loader=file_loader)
 import yaml
-template = env.get_template('/vendor_templates/cisco/iosxe_17/iosxe.j2')
 
-def render_cfg(file):
-    with open(f'./devices_vars/{file}') as info2:
+
+
+def load_templ(vendor):
+    os.chdir('./vendor_templates/')
+    if vendor == 'cisco':
+        file = ('cisco/cisco.j2')
+        template = env.get_template(file)
+        return template
+    elif vendor == 'fortinet':
+        file = ('fortinet/fortios.j2')
+        template = env.get_template(file)
+        return template
+    elif vendor == 'paloalto':
+        file = ('paloalto/panos.j2')
+        template = env.get_template(file)
+        return template
+    elif vendor == 'mikrotik':
+        file = ('mikrotik/routeros.j2')
+        template = env.get_template(file)
+        return template
+    else:
+        file = ('cisco/cisco.j2')
+        template = env.get_template(file)
+        return template
+
+def render_cfg(vendor):
+    template = load_templ(vendor)
+    with open(f'../devices_vars/{vendor}.yml') as info2:
         device_dict = yaml.load(info2, Loader=yaml.FullLoader)
-        #Opens the host device dictionary and pulls the values
-        hostname = (device_dict['hostname'])
         #This will take the hostfile file variables and run through the jinja2 file and output a yaml file
-        leaf_config = template.render(device_dict, undefined=StrictUndefined)
-        #Create the leaf yaml file
-        leaf_file = hostname+".yml"
-        print(leaf_file)
+        vendor_config = template.render(device_dict, undefined=StrictUndefined)
+        #Create the config file
+        vendor_file = vendor+".yml"
+        print(vendor_file)
 
-    with open("./sample_config/"+leaf_file, 'w+') as leafcfg:
-        leafcfg.write(leaf_config)
-        leafcfg.close()
+    with open("../sample_config/"+vendor_file, 'w+') as ipseccfg:
+        ipseccfg.write(vendor_config)
+        ipseccfg.close()
 
     #Print the output
-        print(leaf_config)
+        print(vendor_config)
 
 if __name__ == '__main__':
-    file = sys.argv[1]
-    render_cfg(file)
+    print(os.getcwd())
+    vendor = sys.argv[1]
+    render_cfg(vendor)
--- glue_scripts/vars_builder.py
+++ glue_scripts/vars_builder.py
@@ -1,58 +1,33 @@
 # Louis DeVictoria
+# Script to build the variables file
 
-import ipaddress
 import click
 import yaml
+from render import render_cfg
+import time
+import subprocess
 
 ipsec_params = {}
 
-# Function to validate IP Address
-def isip(ctx, param, address):
-    try:
-        if ipaddress.ip_address(address).is_global == True:
-            return address
-        else:
-            raise click.BadParameter("IP Must be Public")
-    except ValueError:
-        raise click.BadParameter("Re-enter IPv4 Address ex 1.1.1.1 ")
-
-#Function to validate ip prefix
-def isnet(ctx, param , prefix):
-    try:
-        if ipaddress.IPv4Network(prefix) != True:
-            return prefix
-        else:
-            raise click.BadParameter(f"Enter a valid subnet ex 10.0.0.0/24 {prefix}")
-    except ValueError:
-        raise click.BadParameter("Enter a valid subnet ex 10.0.0.0/24 ")
-
-
-
 @click.group()
-
 def cli():
     pass
 
-
 @click.command()
-@click.option("--vendor", prompt=True,
-              type=click.Choice(['cisco', 'palo', 'fortinet', 'mikrotik'], case_sensitive=False, ))
-@click.option("--prem_ip", prompt="Premise IP Address", callback=isip, help="Public IP Client Side.")
-@click.option("--prem_net", prompt="Premise Subnet", callback=isnet, type=str )
-@click.option("--p81_gw", prompt="VPN Name", help="Name of the VPN Perimter81.")
-@click.option("--p81_ip", prompt="Gateway IP", callback=isip, help="P81 Gateway IP.")
-@click.option("--p81_net", prompt="P81 Subnet", callback=isnet, type=str )
-@click.option("--psk", prompt="Preshare Key", help="Preshare Key.")
-@click.option("--encry", prompt=True,
-              type=click.Choice(["3des", "blowfish128", "blowfish192", "blowfish256", "aes128", "aes192", "aes256"],
-                                case_sensitive=False, ))
-@click.option("--integ", prompt=True, type=click.Choice(['md5', 'sha1', 'sha256', 'sha384'], case_sensitive=False, ))
-@click.option("--dhg", prompt=True, type=click.Choice(['2', '5', '14', '19', '20', '21'], case_sensitive=False, ))
+@click.option("--vendor", prompt=True, type=click.Choice(['cisco' , 'paloalto' , 'fortinet' , 'mikrotik'], case_sensitive=False,))
+@click.option("--prem_ip", prompt="Premise IP Address", help="The person to greet.")
+@click.option("--prem_net", prompt="Premise Subnet", help="The person to greet.")
+@click.option("--p81_gw", prompt="VPN Name", help="The person to greet.")
+@click.option("--p81_ip", prompt="Gateway IP", help="The person to greet.")
+@click.option("--p81_net", prompt="P81 Subnet", help="The person to greet.")
+@click.option("--psk", prompt="Preshare Key", help="The person to greet.")
+@click.option("--encry",prompt=True, type=click.Choice(["3des", "blowfish128", "blowfish192", "blowfish256", "aes128", "aes192", "aes256"], case_sensitive=False, ))
+@click.option("--integ",prompt=True, type=click.Choice(['md5', 'sha1', 'sha256', 'sha384'], case_sensitive=False, ))
+@click.option("--dhg",prompt=True, type=click.Choice(['2','5','14','19','20','21'], case_sensitive=False, ))
 @click.option("--ph1_life", prompt="Phase 1 Lifetime", required=True, type=int)
 @click.option("--ph2_life", prompt="Phase 2 Lifetime", required=True, type=int)
 @click.option("--dpd", prompt="Dead Peer Timer: ", required=True, type=int)
-@click.option("--dpd", prompt="Dead Peer Timer: ", required=True, type=int)
-def collect(vendor, prem_ip, prem_net, p81_gw, p81_ip, p81_net, psk, encry, integ, dhg, ph1_life, ph2_life, dpd):
+def collect(vendor,prem_ip,prem_net,p81_gw,p81_ip,p81_net,psk,encry,integ,dhg,ph1_life,ph2_life,dpd):
     ipsec_params['vendor'] = vendor
     ipsec_params['prem_ip'] = prem_ip
     ipsec_params['prem_net'] = prem_net
@@ -67,15 +42,18 @@ def collect(vendor, prem_ip, prem_net, p81_gw, p81_ip, p81_net, psk, encry, inte
     ipsec_params['ph2_life'] = ph2_life
     ipsec_params['dpd'] = dpd
     print(ipsec_params)
-    with open(f'./devices_vars/{vendor}.yml', "w+") as file:
-        yaml.dump(ipsec_params, file, sort_keys=False)
-        return ipsec_params
+    with open(f'./devices_vars/{vendor}.yml',"w+") as file:
+        yaml.dump(ipsec_params,file,sort_keys=False)
+        file.close()
+    time.sleep(5)
+    render_cfg(vendor)
+    return ipsec_params
 
 
-# def hello(count, vendor):
-#    """Simple program that greets NAME for a total of COUNT times."""
-#    for _ in range(count):
-#        click.echo(f"You Selected, {vendor}!")
+
+
+
+
 
 
 if __name__ == '__main__':
