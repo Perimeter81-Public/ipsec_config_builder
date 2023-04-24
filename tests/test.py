d206ffece2e35efe1aed943314db7979eae10e62
louis.devictoria <telescope40@gmail.com>
2023-04-24 15:47:13 -0400

    update test

--- tests/test.py
+++ tests/test.py
@@ -2,12 +2,14 @@
 # Click Testing
 
 from click.testing import CliRunner
-from scripts.vars_builder import cli, collect,render_cfg
+from scripts.vars_builder import cli, collect
+from scripts.render import render_cfg
 
 def test_cli():
 	runner = CliRunner()
-	result = runner.invoke(collect,[], input='cisco\n1.1.1.1\n10.0.0.0/16\nPerimeter81\n2.2.2.2\n10.255.0.0/16\nPRESHARE\naes256\nsha256\n14\n8\n1\n10\ntrue\n65000\n169.254.1.1\n65100\n169.254.1.2\n')
+	result = runner.invoke(collect, input='cisco\n1.1.1.1\n10.0.0.0/16\nPerimeter81\n2.2.2.2\n10.255.0.0/16\nPRESHARE\naes256\nsha256\n14\n8\n1\n10\ntrue\n65000\n169.254.1.1\n65100\n169.254.1.2\n')
+	print(result.stdout)
 	return result
-	#assert not result.exception
+
 
 
